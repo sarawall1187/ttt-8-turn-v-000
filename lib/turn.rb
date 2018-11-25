691a47ac323497ede8478258d2ad45c00085b0da
Sara Walker <sarawall1187@gmail.com>
2018-11-25 18:39:09 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -34,7 +34,8 @@ def turn(board)
   index = input_to_index(user_input)
   if index == valid_move?(board, index)
     move
-  #elsif  
+  elsif valid_move?(board, index) == position_taken?(board, index)
+  puts ""
    
  end
 end
